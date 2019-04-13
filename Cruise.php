d6e1a91e598ddc40318804c82d5b33470593bc45
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-04-13 09:27:31 +0200

    Initial commit modal

---
+++ Cruise.php
@@ -0,0 +1,37 @@
+<?php
+
+class Cruise
+{
+	private static $instance;
+	public $db;
+	public $singular;
+	public $path;
+	public $plural;
+	public $customFields;
+	public $nameSpace;
+	public $icon;
+
+	public function __construct($singular, $plural, $path, $customFields)
+	{
+		$this->singular = $singular;
+		$this->plural = $plural;
+		$this->db = 'bs-cruise';
+		$this->nameSpace = 'bs_cruise';
+		$this->path = $path;
+		$this->icon = 'dashicons-editor-removeformatting';
+		$this->customFields = $customFields;
+	}
+
+	public static function getInstance($singular, $plural, $path, $customFields)
+	{
+		if (!self::$instance instanceof self) {
+			self::$instance = new self($singular, $plural, $path, $customFields);
+		}
+		return self::$instance;
+	}
+
+}
+
+
+
+
